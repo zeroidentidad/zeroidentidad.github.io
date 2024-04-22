cd1e2905a7d0d817efb4c4411fcd3b7f6bf68db8
zeroidentidad <zeroidentidad@alwaysdata.net>
2024-04-22 11:45:00 -0600

    update site

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir(".")))
+	log.Println("running...")
 	http.ListenAndServe(":8080", nil)
 }
